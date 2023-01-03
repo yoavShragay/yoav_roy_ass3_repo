430737d59265dc6d6645d4479039bf0862e2f8db
yoav <yoavshra@gmail.com>
2023-01-04 00:14:29 +0200

    changing vectorData.cpp and vectorData.h

--- vectorData.cpp
+++ vectorData.cpp
@@ -1,34 +1,58 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "vectorData.h"
 using namespace std;
-class vectorData{
-private:
-    string distance;
-    string vec;
-    int k;
-public:
-    vectorData(string distance,string vec,int k){
-        this->distance=distance;
-        this->vec=vec;
-        this->k=k;
-    }
-    string getDistance(){
-        return distance;
-    }
-    string getVec(){
-        return vec;
-    }
-    int getK(){
-        return k;
-    }
-    void setDistance( string distance){
-        this->distance=distance;
-    }
-    void setVec(string vec){
-        this->vec=vec;
-    }
-    void setK(int k){
-        this->k=k;
-    }
-};
+
+/**
+ *
+ * @param distance
+ * @param vec
+ * @param k
+ */
+vectorData::vectorData(string distance,string vec,int k) {
+    this->distance = distance;
+    this->vec = vec;
+    this->k = k;
+}
+/**
+ *
+ */
+string vectorData::getDistance() {
+    return this->distance;
+}
+
+/**
+ *
+ */
+string vectorData::getVec(){
+    return vec;
+}
+
+/**
+ *
+ */
+int vectorData::getK(){
+    return k;
+}
+
+/**
+ *
+ */
+void vectorData::setDistance( string distance){
+    this->distance=distance;
+}
+
+/**
+ *
+ */
+void vectorData::setVec(string vec){
+    this->vec=vec;
+}
+
+/**
+ *
+ */
+void vectorData::setK(int k){
+    this->k=k;
+}
